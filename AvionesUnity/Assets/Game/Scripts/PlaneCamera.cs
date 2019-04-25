8fcc127769396f61b274a012a0f23bb9bab1ef5b
mbpmm <marianomcr92@gmail.com>
2019-04-25 20:40:15 -0300

    Arreglado movimiento de la camara

--- AvionesUnity/Assets/Game/Scripts/PlaneCamera.cs
+++ AvionesUnity/Assets/Game/Scripts/PlaneCamera.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class PlaneCamera : MonoBehaviour
 {
+    /*
+     Primero vos le estas seteando 
+transform.rotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.rotation.z));
 
-    public float yawSpeed;
+Ahi estas mezclando Eulers con Quaternions. Ese "transform.rotation.z" del final está tomando la data del quaternion, no de los euler. Recordá que Quaternion y 
+Euler funcionan completamente diferente.
+
+Ahora, si vos transformás ese "transform.rotation.z" en "transform.rotation.eulerAngles.z" vas a ver que cuado haces un Yaw se empieza a romper todo el control de tu avión. 
+Fijate que pasa con las rotaciones en el Inspector de Unity.
+
+Tratá de observar un poco las rotaciones del ejercicio que vimos en clase. Pensar un poco lo que vimos en clase sobre los pinos tambien, el concepto de una vez que roto mi 
+transform que pasa con la siguiente rotación que hago.
+
+Probá agregar rotación a la rotación que ya tenes y NO setearla en base a tu input. Porque de momento estás usando la posición del mouse en pantalla para definir la rotación de tu nave. 
+Recordá que tenés Input.GetAxis("Mouse Y") tambien:
+https://docs.unity3d.com/ScriptReference/Input.GetAxis.html 
+
+
+Por otro lado:
+transform.eulerAngles += new Vector3(0,  horizontal * yawSpeed * Time.deltaTime, 0);
+supongo que vendría a ser:
+transform.eulerAngles += new Vector3(0, 0, horizontal * yawSpeed * Time.deltaTime);
+no?
+     */
+
+
+    public float rollSpeed;
     public float planeSpeed;
     public float planeAcc;
+    public float sensX;
+    public float sensY;
 
     private float mouseX;
     private float mouseY;
     private float horizontal;
-
+    private Vector3 lookRot;
     // Start is called before the first frame update
     void Start()
     {
         horizontal = Input.GetAxis("Horizontal");
-        yawSpeed = 3000f;
+        rollSpeed = 20f;
         planeSpeed = 10f;
+        sensX = 70f;
+        sensY = 70f;
     }
 
     // Update is called once per frame
@@ -27,11 +56,12 @@ public class PlaneCamera : MonoBehaviour
         planeAcc = Input.GetAxis("Vertical");
         planeSpeed += planeAcc;
         horizontal = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.forward * planeSpeed * Time.deltaTime);
-        mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
-        mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
-        transform.rotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.rotation.z));
-        transform.eulerAngles += new Vector3(0,horizontal * yawSpeed * Time.deltaTime, 0);
         
+        transform.Translate(Vector3.forward * planeSpeed * Time.deltaTime);
+        mouseX = Input.GetAxis("Mouse X")*sensX*Time.smoothDeltaTime;
+        mouseY = Input.GetAxis("Mouse Y")*sensY*Time.smoothDeltaTime;
+        lookRot = new Vector3(-mouseY,mouseX, 0);
+        transform.Rotate(lookRot); 
+        transform.eulerAngles += new Vector3(0, 0, -horizontal * rollSpeed * Time.deltaTime);
     }
 }
