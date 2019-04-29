3a09fc6e8e358edd166ff7f2983f1e1751d44951
mbpmm <marianomcr92@gmail.com>
2019-04-29 02:10:00 -0300

    Agregado del script UIManager

---
+++ AvionesUnity/Assets/Game/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text altura;
+    public Text rrs;
+    public Text ammo;
+    private GameObject plane;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.Find("spitfire");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        altura.text = "Altura: " +Mathf.Round(plane.transform.position.y) + "m.";
+        rrs.text = "RRS: " + Mathf.Round(plane.transform.eulerAngles.z) + "°";
+        ammo.text = "Municion: ∞";
+    }
+}
